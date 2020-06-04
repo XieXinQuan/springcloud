4f3c6589c4e96f957c44e202e3760f95801a27ec
XieXinQuan <xiexinquan520@163.com>
2020-06-04 20:08:41 +0800

    添加持久化和邮件发送

--- Message8011/src/main/java/com/quan/Message8011.java
+++ Message8011/src/main/java/com/quan/Message8011.java
@@ -3,6 +3,7 @@ package com.quan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author: xiexinquan520@163.com
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class Message8011 {
     public static void main(String[] args) {
         SpringApplication.run(Message8011.class, args);
--- Message8011/src/main/java/com/quan/message/DirectReceiver.java
+++ Message8011/src/main/java/com/quan/message/DirectReceiver.java
@@ -1,9 +1,20 @@
 package com.quan.message;
 
+import com.alibaba.fastjson.JSONObject;
+import com.quan.Enum.CommonByteEnum;
+import com.quan.Enum.EmailType;
+import com.quan.entity.FailureEmail;
+import com.quan.entity.SendMessage;
+import com.quan.repository.FailureEmailRepository;
+import com.quan.repository.SendMessageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -13,16 +24,53 @@ import java.util.Map;
  */
 @Component
 @RabbitListener(queues = "QuanDirectQueue")//监听的队列名称 TestDirectQueue
+@Slf4j
 public class DirectReceiver {
+    @Resource
+    SendMessageRepository sendMessageRepository;
+    @Value("${spring.mail.username}")
+    String from;
+    @Resource
+    JavaMailSender mailSender;
+    @Resource
+    FailureEmailRepository failureEmailRepository;
 
     @RabbitHandler
-    public void process(Map<String, Object> testMessage) {
-        Long id = Long.parseLong(testMessage.get("id").toString());
-        if (MessageUtil.contains(id)) {
-            System.out.println("消息重复, 重复内容:" + testMessage.toString());
+    public void process(Map<String, Object> messageMap) {
+        if (messageMap == null) return;
+        Long id = Long.parseLong(messageMap.get("id").toString());
+        //先判断缓存有没有， 再判断数据库
+        if (MessageUtil.contains(id) || sendMessageRepository.findByMessageId(id) != null) {
+            log.info("消息重复, 重复内容:{}", messageMap.toString());
             return;
         }
         MessageUtil.add(id);
-        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+        //发送消息 -- 入库
+        SendMessage sendMessage = new SendMessage();
+        try{
+            sendMessage.setMessageId(Long.parseLong(messageMap.get("id").toString()));
+            sendMessage.setStatus(CommonByteEnum.Normal.getKey());
+            sendMessage.setReceiveUserId(Long.parseLong(messageMap.get("userId").toString()));
+            sendMessage.setEmailAddress(messageMap.get("emailAddress").toString());
+            sendMessage.setType(Byte.parseByte(messageMap.get("type").toString()));
+            sendMessage.setMessage(messageMap.get("message").toString());
+            sendMessageRepository.save(sendMessage);
+            String title = "";
+            EmailType[] values = EmailType.values();
+            for (EmailType emailType : values){
+                if (sendMessage.getType().equals(emailType.getKey())) title = emailType.getValue();
+            }
+            title += "通知";
+
+//            EmailUtil.sendEmail(mailSender, from, sendMessage.getEmailAddress(), title, sendMessage.getMessage(), null);
+        }catch (Exception exception){
+            FailureEmail failureEmail = new FailureEmail();
+            failureEmail.setContent(JSONObject.toJSONString(messageMap));
+            failureEmail.setExceptionType(exception.getClass().getName());
+            failureEmail.setExceptionReason(exception.getMessage());
+            failureEmailRepository.save(failureEmail);
+            log.info("exception", exception);
+        }
+
     }
 }
